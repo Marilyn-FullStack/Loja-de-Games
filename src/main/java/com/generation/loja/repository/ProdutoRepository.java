c74dc62ca57dbccc3af99f78e9d9179787374fc2
Maria <m.leiliane.lobo@gmail.com>
2023-12-14 16:53:17 -0300

    projeto final

--- src/main/java/com/generation/loja/repository/ProdutoRepository.java
+++ src/main/java/com/generation/loja/repository/ProdutoRepository.java
@@ -12,6 +12,4 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>  {
 	public List <Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 	List<Produto> findByPrecoLessThan(BigDecimal valor);
 	List<Produto> findByPrecoGreaterThan(BigDecimal valor);
-	
-
 }
