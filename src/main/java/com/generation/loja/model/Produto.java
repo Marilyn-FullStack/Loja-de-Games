238bba600cf022187d60919ef0ff12a9010a09ac
Maria <m.leiliane.lobo@gmail.com>
2023-12-14 14:53:48 -0300

    first commit

---
+++ src/main/java/com/generation/loja/model/Produto.java
@@ -0,0 +1,76 @@
+package com.generation.loja.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    @NotBlank(message = "O atributo nome é Obrigatório!")
+    @Size(min = 5, max = 50, message = "O atributo nome deve conter no mínimo 5 e no máximo 50 caracteres")
+    private String nome;
+
+    @Column(length = 10)
+    @NotNull(message = "O atributo preço é Obrigatório!")
+    @DecimalMin(value = "1.0", message = "O atributo preço deve ser maior ou igual a 1.0")
+    @DecimalMax(value = "10.0", message = "O atributo preço deve ser menor ou igual a 10.0")
+    private BigDecimal preco;
+    
+    @UpdateTimestamp
+    private LocalDateTime date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+    
+}
+
